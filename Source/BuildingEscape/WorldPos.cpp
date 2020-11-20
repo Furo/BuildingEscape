f8667a748f3622ef2a86a677ed0e87e8a08d2c12
Furo <anthonypi0179@gmail.com>
2020-11-20 08:31:41 -0600

    Pointer/refrence practice

---
+++ Source/BuildingEscape/WorldPos.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPos.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPos::UWorldPos()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPos::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//FString ObjectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Object name is: %s"), *GetOwner()->GetName());
+
+	// FString Log = TEXT("Hello!");
+	// FString* PrtLog = &Log;
+	// Log.Len();
+	// (*PrtLog).Len();
+	// PrtLog->Len();
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), **PrtLog);
+
+
+}
+
+
+// Called every frame
+void UWorldPos::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
